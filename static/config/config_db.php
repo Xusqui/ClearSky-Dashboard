2d7d9b7be8723465524751629cd29820639c977d
Xusqui <xusqui@gmail.com>
2025-10-18 22:26:44 +0200

    Cambiar inser.php y borrar configuración sensible

--- static/config/config_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Datos de conexión a MariaDB
-// Añadido a .gitignore
-$db_user = ""; // DataBase User
-$db_pass = ""; // DataBase Password
-$db_url = ""; // dadtabase url
-$db_database = ""; // DataBase name
-?>
