08000c35e2cfc09e7df668ac9674807e9e070ca4
Xusqui <xusqui@gmail.com>
2025-10-18 10:40:25 +0200

    Delete static/config/config_db.php

--- static/config/config_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Datos de conexión a MariaDB
-$db_user = ""; //Usuario de la base de datos
-$db_pass = "#"; // Database password
-$db_url = "127.0.0.1"; // Database host
-$db_database = ""; // Database name
-?>
