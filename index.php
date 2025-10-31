c8e163065164d4ec5640e9d705de5cde3cd460b8
Xusqui <xusqui@gmail.com>
2025-10-31 20:21:37 +0100

    Pequeño cambio tonto

--- index.php
+++ index.php
@@ -1060,7 +1060,7 @@ if (isset($data["last_updated"])) {
                             <div class="modal-content">
                                 <button class="close" id="closeMoonModal" aria-label="Cerrar">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
-                                        stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
+                                         stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
                                         <line x1="18" y1="6" x2="6" y2="18" />
                                         <line x1="6" y1="6" x2="18" y2="18" />
                                     </svg>
