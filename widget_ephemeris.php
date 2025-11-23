6aefda1069288a6f93fbe2009fbc8a11c5278caf
Xusqui <xusqui@gmail.com>
2025-11-23 17:44:54 +0100

    Faltaba por subir un archivo de las efemérides celestes

---
+++ widget_ephemeris.php
@@ -0,0 +1,18 @@
+<?php
+// widget_ephemeris.php
+?>
+<div class="big-card-ephemeris ephemeris-card" id="ephemeris-widget">
+    <div class="ephemeris-card-container">
+        <div class="ephemeris-main-row">
+            <div class="ephemeris-icon">
+                <img src="./static/images/saturn.png" alt="Telescopio" class="ephemeris-img-icon">
+            </div>
+        </div>
+        <div class="ephemeris-title">
+            <p id="ephemeris-text">Efemérides Astronómicas</p>
+        </div>
+        <div class="ephemeris-subtitle">
+            <p id="ephemeris-today">Click para ver eventos de hoy</p>
+        </div>
+    </div>
+</div>
